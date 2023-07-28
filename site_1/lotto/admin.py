6a133002036809f0b10be07f87b59a42ca853930
NakHyun Choi <9naknak8@gmail.com>
2023-07-28 11:59:01 +0900

    again

--- site_1/lotto/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from lotto.models import GuessNumbers
-
-# Register your models here.
-# 만들었던 테이블 등록
-admin.site.register(GuessNumbers)
--- site_1/lotto/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import GuessNumbers
-
-class PostForm(forms.ModelForm):
-    
-    # 상위의 라는 뜻
-    class Meta:
-        model = GuessNumbers
-        fields = ('name', 'text','test')
